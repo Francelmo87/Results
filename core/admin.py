711b930d4283c65c70fdd7b9b4c4fa4d31859d0b
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-03-19 19:34:15 -0300

    criado a parte administrativa

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Result
+
 
 # Register your models here.
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'user', 'protocol', 'name', 'number']
+    list_display = ['id', 'user', 'protocol', 'name', 'number', 'result_1', 'result_2', 'active']
