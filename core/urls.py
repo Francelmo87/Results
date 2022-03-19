0d7c01f51d20c130f1a91a83ed79832de306eded
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-03-19 18:22:01 -0300

    criada a view e path de login

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from core import views as v
 
 app_name = 'core'
 
-
 urlpatterns = [
-        path('', v.index, name='index'),
+    path('', v.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 def index(request):
     template = 'index.html'
     return render(request, template)
+
+
+def login(request):
+    template = 'longin.html'
+    return render(request, template)
+
