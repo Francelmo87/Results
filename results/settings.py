df200f15f68bec75dd2d87a51bb73a19b26115b3
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-03-19 23:36:35 -0300

    Adicionada os caminhos de login e logout

--- results/settings.py
+++ results/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-i*$wk5w9=tx+^)y2$mul=pt_4y$&)khe#e&c8(e!)&%mss*z_$
 DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'django-results.herokuapp.com']
-
 
 # Application definition
 
@@ -40,8 +38,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'core',
 ]
-
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'results.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -83,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -103,18 +97,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/fortaleza'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -125,3 +117,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'  # Redireciona para a tela de login
+LOGIN_REDIRECT_URL = '/list/'  # Em caso de sucesso redireciona para a página escolhida
+LOGOUT_REDIRECT_URL = '/'  # pagina que levada ao sair
