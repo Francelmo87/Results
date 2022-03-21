d0d0bbab75e8a32d0d94107779a7c86b0b0c8e7d
Francelmo Sousa da Silva <francelmo87@hotmail.com>
2022-03-20 22:27:31 -0300

    alteração para o pdf abrir em outra aba

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('list/', v.result_list, name='list'),
-    path('pdf/', GeneratePdf.as_view()),
+    path('pdf/', GeneratePdf.as_view(), name='pdf'),
 
 ]
